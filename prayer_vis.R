cf6ae9f157f645678b05ec17eb73dbb130d72139
IrfanZM <mohamedizb@gmail.com>
2019-06-15 23:03:39 +0100

    Formatted columns to date-time format, created first draft visualisation

--- prayer_vis.R
+++ prayer_vis.R
@@ -1,20 +1,42 @@
 library(dplyr)
-library(stringr)
 library(lubridate)
 library(tidyr)
+library(stringr)
 library(zoo)
 
 #Load in prayer times and convert to tidy format
 prayer_times <- read.csv("./data/prayertimes.csv", stringsAsFactors = FALSE)
 
-#Fix missing values to use last valid value
+#Fix missing values to use last valid value, add dot in time value
 for (col in names(prayer_times)) {
   prayer_times[,col][prayer_times[,col]=="** **"] = NA
   prayer_times[,col] = na.locf(prayer_times[,col])
+  prayer_times[,col] = gsub(" ",":",prayer_times[,col])
 }
-
-
 
 #Change to long data format/tidy format
 prayer_times <- gather(data=prayer_times, 'Twilight','Sunrise','Transit','Shadow.1','Shadow.2','Sunset','Twilight.1',
-                       key = 'time', value = 'value')
+                       key = 'name', value = 'time')
+
+#Concatenate columns and split out date and time for plotting
+prayer_times$date <- paste(prayer_times$Year, prayer_times$mth, prayer_times$d, sep="-")
+prayer_times$date_time <- ymd_hm(paste(prayer_times$date, prayer_times$time, sep="T"))
+
+prayer_times <- prayer_times %>%
+  select(-c("time","date")) %>%
+  mutate(time=format(date_time, format="%H;%M:%S")) %>%
+  mutate(time=as.POSIXct(time, format="%H;%M:%S"))
+
+#Plot onto line chart
+prayer_chart <- ggplot(data=prayer_times,aes(x=date_time,y=time,colour=name)) +
+  geom_line()
+
+############################
+#To debug:
+#Order of each prayer in the graph
+#Line jumping from top to bottom - sort out y-axis and labels
+#Date showing on left hand column - there must be a better way of showing time
+#General visual touch-ups
+
+
+

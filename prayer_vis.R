c7115676daa3c6d21590aec5525fa0a37b22b8bf
IrfanZM <mohamedizb@gmail.com>
2019-06-16 17:39:23 +0100

    Tweaked chart output and names.

--- prayer_vis.R
+++ prayer_vis.R
@@ -15,17 +15,19 @@ for (col in names(prayer_times)) {
   prayer_times[,col] = gsub(" ",":",prayer_times[,col])
 }
 
-#Change to long data format/tidy format
-prayer_times <- gather(data=prayer_times, 'Twilight','Sunrise','Transit','Shadow.1','Shadow.2','Sunset','Twilight.1',
+#Rename columns and change to long data format/tidy format
+#Add factor levels for prayer names
+names(prayer_times) <- c("Year","mth","d","Fajr","Sunrise","Dhuhr","Asr.1","Asr.2","Sunset","Isha")
+prayer_times <- gather(data=prayer_times, 'Fajr','Sunrise','Dhuhr','Asr.1','Asr.2','Sunset','Isha',
                        key = 'name', value = 'time')
+prayer_times$name <- factor(x=prayer_times$name,c("Isha","Sunset","Asr.2","Asr.1","Dhuhr","Sunrise","Fajr"))
 
 #Add helper column to move any 24+ hr times to next day
 prayer_times$next_day <- ifelse(as.numeric(str_sub(prayer_times$time,1,2)) >= 24,1,0)
 
-#Concatenate columns and split out date and time for plotting
+#Concatenate columns and split out date and time for plotting, tidy up table
 prayer_times$date <- paste(prayer_times$Year, prayer_times$mth, prayer_times$d, sep="-")
 prayer_times$date_time <- ymd_hm(paste(prayer_times$date, prayer_times$time, sep="T"))
-
 prayer_times <- prayer_times %>%
   mutate(time=format(date_time, format="%H;%M:%S")) %>%
   mutate(time=as.POSIXct(time, format="%H;%M:%S") + (prayer_times$next_day * 24* 3600)) %>%
@@ -33,12 +35,16 @@ prayer_times <- prayer_times %>%
 
 #Plot onto line chart
 prayer_chart <- ggplot(data=prayer_times,aes(x=date_time,y=time,colour=name)) +
-  geom_line()
+  geom_line() +
+  scale_x_datetime(name="Month",date_labels="%b", date_breaks = "1 month", date_minor_breaks = "1 month") + 
+  scale_y_datetime(name="Time",date_labels="%H:%M", date_breaks = "1 hours", date_minor_breaks = "1 hour") +
+  scale_colour_discrete(name="",label=c("Isha","Sunset","Asr 2","Asr 1","Dhuhr","Sunrise","Fajr"))
+
+#Write image to file
+ggsave("./outputs/prayer_chart.png",device = "png",height = 12, width = 18, units="cm", dpi=600)
 
 ############################
 #To debug:
-#Order of each prayer in the graph
-#Date showing on left hand column - there must be a better way of showing time
 #General visual touch-ups
 
 
